187893d277258779921c4b154943c8b15fbe0e1f
Hazama25 <hazamafawkes@gmail.com>
2021-06-28 05:36:24 +0000

    surya: Enigma-fy
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- colt_surya.mk
+++ colt_surya.mk
@@ -4,24 +4,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit device configuration
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
-
+# Screen Resolution
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Inherit some common Colt-Enigma stuff
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+COLT_BUILD_TYPE := Official
+COLT_BUILD_MAINTAINER := Hazama25
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_surya
+PRODUCT_NAME := colt_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
-DEVICE_MAINTAINER := Ganesh Varma
 
 BUILD_FINGERPRINT := POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.3.0.QJGMIXM:user/release-keys
-TARGET_INCLUDE_PIXEL_CHARGER := true
